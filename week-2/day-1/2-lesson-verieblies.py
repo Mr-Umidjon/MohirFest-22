cac71c7485a3e3f0af582c92cd413463c29dcd61
Umidjon <mrumidjon2020@gmail.com>
2022-11-21 18:09:26 +0500

    Variebles

---
+++ week-2/day-1/2-lesson-verieblies.py
@@ -0,0 +1,14 @@
+name = "Umidjon"
+print(name)
+
+last_name = "Maxammadsoliyev"
+print(last_name)
+
+print(name + last_name)
+
+a, b, c = 1, 2, 3
+
+print(a, b, c)
+
+text = "Python is the best programming language."
+print(text)
