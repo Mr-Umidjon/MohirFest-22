9550e9b3d58c39d48c3ae4334b25aec1fe80f473
Umidjon <mrumidjon2020@gmail.com>
2022-11-21 18:23:41 +0500

    data types

---
+++ week-2/day-1/3-lesson-data-types.py
@@ -0,0 +1,32 @@
+int_num = 1212
+print(int_num)
+print("-"*10)
+
+float_num = 12.34
+print(float_num)
+print("-"*10)
+
+text = 'My name is Umidjon'
+print(text)
+print("-"*10)
+
+list1 = [1, 'two', 'three', 4, 5, 6.0]
+print(list1)
+print("-"*10)
+
+tuple = (1, 2, 3, 4, 'five')
+print(tuple)
+print("-"*10)
+
+set1 = {1, 1, 2, 1, 2, 3, 4, 3, 4}
+print(set1)
+print("-"*10)
+
+dict1 = {'name': "Umidjon", 'age': 20}
+print(dict1)
+print("-"*10)
+
+
+boolean = True
+print(boolean)
+print("-"*10)
