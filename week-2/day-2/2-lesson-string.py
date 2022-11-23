4e14b8a84d47f336bccd24fd7b70a525ceea4c91
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 11:56:16 +0500

    string

--- week-2/day-2/2-lesson-string.py
+++ week-2/day-2/2-lesson-string.py
@@ -5,3 +5,28 @@ print(text[:2])
 print(text[3:7])
 print(text[8:10])
 print(text[11:])
+print("-"*100)
+
+print(text[-7:])
+print("-"*100)
+
+
+text_len = len(text)
+print(text_len)
+print("-"*100)
+
+text = text.upper()
+print(text)
+print("-"*100)
+
+text = text.lower()
+print(text)
+print("-"*100)
+
+text = text.title()
+print(text)
+print("-"*100)
+
+text = text.replace("Umidjon", "Odiljon")
+print(text)
+print("-"*100)
