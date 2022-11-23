d61968be0beba4167d99b77a3416eae8b7e8834b
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 11:46:32 +0500

    numbers

--- week-2/day-2/1-lesson-numbers.py
+++ week-2/day-2/1-lesson-numbers.py
@@ -5,8 +5,16 @@ float_num = 12.5
 print(int_num)
 print(type(int_num))
 
-print(100*"-")
+print(100 * "-")
 print(float(int_num))
 
-print(100*"-")
+print(100 * "-")
 print(int(float_num))
+
+import random
+
+random_num = random.randrange(1, 100)
+
+print(100 * "-")
+print(random_num)
+print(type(random_num))
