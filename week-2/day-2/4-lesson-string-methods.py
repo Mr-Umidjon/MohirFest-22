ec4ba89fdc9be688ed07fbe5f820e78d81b709ef
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 15:25:00 +0500

    string methods

---
+++ week-2/day-2/4-lesson-string-methods.py
@@ -0,0 +1,24 @@
+text = 'My name is Umidbek'
+
+print(text.isalpha())
+print(100*'-')
+
+
+print(text.isdigit())
+print(100*'-')
+
+list1 = ['My', 'name', 'is', "Umidbek"]
+
+print(" ".join(list1))
+print(100*'-')
+
+print(text.startswith('My'))
+print(100*'-')
+
+print(text.endswith('bek'))
+print(100*'-')
+
+num = '9'
+
+print(num.zfill(5))
+print(100*'-')
