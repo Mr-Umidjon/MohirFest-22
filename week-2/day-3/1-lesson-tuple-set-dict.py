040bcc62a152c271d55537a2dde672906a180167
Umidjon <mrumidjon2020@gmail.com>
2022-11-24 11:48:48 +0500

    tuple set dict

--- week-2/day-3/1-lesson-tuple-set-dict.py
+++ week-2/day-3/1-lesson-tuple-set-dict.py
@@ -5,3 +5,23 @@ print(100*"-")
 
 print(fruits[0])
 print(100*"-")
+
+print(len(fruits))
+print(100*"-")
+
+print(type(fruits))
+print(100*"-")
+
+boolean = (True, False, True)
+
+print(boolean)
+print(100*"-")
+
+for fruit in fruits:
+    print(fruit)
+print(100*"-")
+
+set_num = {1, 3, 2, 1, 4, 4, 3}
+print(set_num)
+print(100*"-")
+
