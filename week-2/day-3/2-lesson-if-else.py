a830d81bec0affd9e6efdd834f38a0267f18ab0c
Umidjon <mrumidjon2020@gmail.com>
2022-11-24 18:24:16 +0500

    if else

--- week-2/day-3/2-lesson-if-else.py
+++ week-2/day-3/2-lesson-if-else.py
@@ -1,8 +1,14 @@
-a = 3
+a = 6
 b = 5
 
 print(a == b)
 print(100 * '-')
 
 print(a != b)
+print(100 * '-')
+
+if a > b:
+    print("a is bigger than b")
+else:
+    print('b is bigger than a')
 print(100 * '-')
