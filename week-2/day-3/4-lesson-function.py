e1e249b4919de60d7626303625ff9188449d3294
Umidjon <mrumidjon2020@gmail.com>
2022-11-24 18:35:15 +0500

    function

--- week-2/day-3/4-lesson-function.py
+++ week-2/day-3/4-lesson-function.py
@@ -3,3 +3,26 @@ def say_hello():
 
 
 say_hello()
+
+print(100 * '-')
+
+
+def say_my_name(name):
+    print(f"my name is {name}")
+
+
+say_my_name('umidjon')
+print(100 * '-')
+
+
+def square(x):
+    sq = x * x
+    return sq
+
+
+print(square(12))
+print(100 * '-')
+
+age = input("How old are you?")
+print(age)
+print(100 * '-')
