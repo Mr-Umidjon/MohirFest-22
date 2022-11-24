a0191e2e7502d744a50092f7964c2ae02b7cbccb
Umidjon <mrumidjon2020@gmail.com>
2022-11-24 18:47:30 +0500

    Classes OOP

--- week-2/day-3/5-lesson-classes-OOP.py
+++ week-2/day-3/5-lesson-classes-OOP.py
@@ -4,3 +4,26 @@ class Class1:
 
 print(Class1)
 print(100 * '-')
+
+object1 = Class1()
+print(object1.num)
+print(100 * '-')
+
+
+class Auto:
+    def __init__(self, model, name, year, price):
+        self.model = model
+        self.name = name
+        self.year = year
+        self.price = price
+
+    def __str__(self):
+        return 'Auto class'
+
+
+spark = Auto("Daewoo", 'Spark', 2006, 2000)
+print(spark.price, spark.year)
+print(100 * '-')
+
+print(spark)
+print(100 * '-')
