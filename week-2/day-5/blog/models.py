32eb42dd472eb8e0ccc60bfa7c1509beeaf2045c
Umidjon <mrumidjon2020@gmail.com>
2022-11-28 11:51:59 +0500

    blog

--- week-2/day-5/blog/models.py
+++ week-2/day-5/blog/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
