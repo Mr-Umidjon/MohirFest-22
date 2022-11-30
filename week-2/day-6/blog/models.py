45ad39933367de3f99c244935153504df0d7181f
Umidjon <mrumidjon2020@gmail.com>
2022-11-30 14:56:44 +0500

    blog

--- week-2/day-6/blog/models.py
+++ week-2/day-6/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -10,3 +11,6 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog_detail_view', args=[str(self.id)])
