21964ff8f74211715fdbef844ffefe4715237592
Umidjon <mrumidjon2020@gmail.com>
2022-12-02 00:14:03 +0500

    telegram bot

---
+++ week-3/day-1/wikibot/main.py
@@ -0,0 +1,41 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5316136617:AAFQykQEy7xZ4GVan_vEQ_h_I8Ywsg3ubFI'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Salom Wikibotga Xush kelibsiz!")
+
+
+
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Bu bot Wikipedia dan foydali ma'lumotlarni olib jo'natadi!")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
